9789925afd13ee9f3bd5c1cb799c6b40ae360478
sacarvy <sukhpreetben10@gmail.com>
2022-10-09 05:39:09 +0530

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { createCount } from './counter';
+	const count = createCount();
+</script>
+
+<button on:click={count.increment}> + </button>
+<h1>
+	{$count}
+</h1>
+<button on:click={count.decrement}> - </button>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
+
+module.exports = config;
